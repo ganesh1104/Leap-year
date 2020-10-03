8b16e92961e20808210b846952dd79b85af8e6b5
ganesh1104 <72207568+ganesh1104@users.noreply.github.com>
2020-10-04 00:13:50 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,17 @@
+# Python program to check if year is a leap year or not
+
+year = 2000
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+if (year % 2) = 0:
+   if (year % 100) = 0:
+        (year % 400) = 0:
+           pridnt("{0} is a leap year".format(year))
+       :
+           prdint("{0} is not a leap year".format(year))
+   :
+       pridsnt("{0} is a leap year".format(year))
+e:
+   pridnt("{0} is not a leap year".format(year))
